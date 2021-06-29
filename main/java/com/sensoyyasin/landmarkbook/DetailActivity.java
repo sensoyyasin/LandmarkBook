a9312a8f85b84f30d81b3dc0bca2e704897d6a22
Yasin Şensoy <73845925+sensoyyasin@users.noreply.github.com>
2021-06-29 10:56:06 +0300

    Add files via upload

---
+++ main/java/com/sensoyyasin/landmarkbook/DetailActivity.java
@@ -0,0 +1,40 @@
+package com.sensoyyasin.landmarkbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import org.w3c.dom.Text;
+//import static com.sensoyyasin.landmarkbook.MainActivity.selectedImage;
+
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        TextView landmarkNameText = findViewById(R.id.landmarkNameText);
+        TextView countryNameText = findViewById(R.id.countryNameText);
+
+        Intent intent = getIntent();
+        String landmarkName = intent.getStringExtra("name");
+        String countryName = intent.getStringExtra("country");
+        landmarkNameText.setText(landmarkName);
+        countryNameText.setText(countryName);
+
+        Singleton singleton = Singleton.getInstance();
+        imageView.setImageBitmap(singleton.getChosenImage());
+
+       //imageView.setImageBitmap(selectedImage);
+
+
+        
+
+    }
+}
